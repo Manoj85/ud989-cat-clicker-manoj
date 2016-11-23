ba5b54d62e9e481d0ea8aefd78d85c113410391c
Manoj Talagadadeevi <manojt.854@gmail.com>
2016-11-22 21:05:26 -0500

    catClicker v3 with 5 cats

--- js/app.js
+++ js/app.js
@@ -1,12 +1,55 @@
 $( function() {
-    var count1 = 0, count2 = 0;
-    $('#clicker1').click(function(e) {
-        count1++;
-        $('#counter1').text(count1);
+
+    var cats = [];
+    $.getJSON("./data/cats.json", function(data) {
+        cats = data;
+        const catListDiv = $("div#catname-box .list");
+        const catTitleDiv = $("div#catTitle-box");
+        const catCounterDiv = $("div#catCounter-box");
+        const catImgDiv = $("div#catimg-box");
+        const catDisplayBox = $("div#cat-display-box");
+
+        /*
+        $.each(cats, function () {
+
+            var img_src = this.image_src;
+            var img_title = this.name;
+            var img_click_count = this.count;
+            var $li = $("<li>")
+                .text(img_title)
+                .addClass("menu-item")
+                .click( {param1: img_src, param2: img_title}, function(event) {
+                    catTitleDiv.empty();
+                    catImgDiv.empty();
+                    catImgDiv.css({"background-image": "url(" + event.data.param1 + ")"});
+
+                    catImgDiv.click(
+
+                    );
+                    catTitleDiv.text(event.data.param2);
+                    catCounterDiv.text(img_click_count);
+                })
+                .appendTo(catListDiv)
+        });
+        */
+
+        const total_cats = cats.length;
+        for (let i = 0; total_cats && i < total_cats; i++) {
+            let cat = cats[i];
+
+            const elem = document.createElement('li');
+            elem.textContent = cat.name;
+
+            console.log(elem);
+
+            catListDiv.appendChild(elem);
+        }
     });
-    $('#clicker2').click(function(e) {
-        count2++;
-        $('#counter2').text(count2);
-    });
+
+
+
+
+
+
 });
 
